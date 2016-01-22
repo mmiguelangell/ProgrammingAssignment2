2b4e487c39ed859f92a1f485bbee0bed41f94d23
mmiguelangell <miguelarcangel@comunidad.unam.mx>
2016-01-21 20:06:11 -0600

    Homework

--- cachematrix.R
+++ cachematrix.R
@@ -7,9 +7,32 @@ makeCacheMatrix <- function(x = matrix()) {
 
 }
 
+x_inversa <- NULL
+set <- function(x_nue) {   ##another matrix x_nue
+	x <<- x_nue
+	x_inversa <<- NULL }
+get <- function() {
+ x 
+} ##obtain matrix
 
-## Write a short comment describing this function
+set_inverse <- function(inv_nue) {  ##other matrix inverse
+	x_inversa <<- inv_nue
+}
+get_inverse <- function() {
+	x_inversa
+}
+list(set=set, get=get,
+set_inverse=set_inverse, get_inverse=get_inverse)
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(cached_x, ...) {
+x_inversa <- cached_x$get_inverse()
+if(!is.null(x_inversa)){
+message("calculation of cache")
+return(x_inversa)
+}
+data <- cached_x$get()
+inv_nue <- solve(data)
+cached_x$set_inverse(inv_nue)
+inv_nue
 }
